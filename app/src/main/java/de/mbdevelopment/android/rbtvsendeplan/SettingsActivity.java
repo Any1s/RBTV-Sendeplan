8bf0f37ad227b116d9cfb84f70263798cb899227
Any1s <rbtv.android.app@gmail.com>
2015-02-08 19:38:26 +0100

    Set action bar color in xml instead of code.

--- app/src/main/java/de/mbdevelopment/android/rbtvsendeplan/SettingsActivity.java
+++ app/src/main/java/de/mbdevelopment/android/rbtvsendeplan/SettingsActivity.java
@@ -1,7 +1,5 @@
 package de.mbdevelopment.android.rbtvsendeplan;
 
-import android.graphics.drawable.ColorDrawable;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 
@@ -15,10 +13,6 @@ public class SettingsActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-        // TODO workaround
-        ActionBar ab = getSupportActionBar();
-        if (ab != null) ab.setBackgroundDrawable(
-                new ColorDrawable(getResources().getColor(R.color.action_bar_background)));
 
         getFragmentManager().beginTransaction()
                 .replace(android.R.id.content, new SettingsFragment())
